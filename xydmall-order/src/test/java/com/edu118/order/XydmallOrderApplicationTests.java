b270ba159c4aef4ab4bcae2e321b1ba2dfd354d7
xiaobo <1225001172@qq.com>
2020-09-15 22:31:24 +0800

    第二次修改的综合练习

---
+++ xydmall-order/src/test/java/com/edu118/order/XydmallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu118.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XydmallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ xydmall-warehouse/src/test/java/com/edu118/warehouse/XydmallWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu118.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XydmallWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
